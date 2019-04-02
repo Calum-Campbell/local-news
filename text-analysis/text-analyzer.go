47b42cbe0daa8bcda5bf25e8804b624fd66ff9a9
Lauren Emms <lauren.emms@guardian.co.uk>
2019-04-02 17:04:17 +0100

    api working

--- text-analysis/text-analyzer.go
+++ text-analysis/text-analyzer.go
@@ -1,20 +1,56 @@
 package main
 
 import (
-	"context"
+	"github.com/aws/aws-sdk-go/service/comprehend"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/pkg/errors"
+	"os"
 	"fmt"
 	"io/ioutil"
-	"os"
 )
 
 func main() {
-	ctx := context.Background()
 
-	fileName := os.Args[0]
+	fileName := os.Args[1]
 
 	fileContent, err := ioutil.ReadFile(fileName) // just pass the file name
 	if err != nil {
 		fmt.Print(err)
 	}
 
+	fileContentAsString := string(fileContent)
+
+	client, err := CreateComprehendClient("identity")
+	fmt.Print(AnalyseText(client, fileContentAsString))
+}
+
+func CreateComprehendClient(profile string) (*comprehend.Comprehend, error) {
+	sess, err := session.NewSession(&aws.Config{
+	Region: aws.String("eu-west-1"),
+	})
+
+	if err != nil {
+	return nil, errors.Wrap(err, "unable to create new sessions")
+	}
+
+	sess.Config.Credentials = credentials.NewCredentials(
+	&credentials.SharedCredentialsProvider{
+	Profile: profile,
+	},
+	)
+
+	if _, err := sess.Config.Credentials.Get(); err != nil {
+	return nil, errors.Wrap(err, "unable to get credentials")
+	}
+
+	return comprehend.New(sess), nil
+}
+
+func AnalyseText(client *comprehend.Comprehend, fileContent string) (*comprehend.BatchDetectSentimentOutput, error) {
+	input := &comprehend.BatchDetectSentimentInput{}
+	input.SetLanguageCode("en")
+	input.SetTextList([]*string{aws.String(fileContent)})
+	return client.BatchDetectSentiment(input)
 }
