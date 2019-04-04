4acf209defe3c7427259985641bc2927851aa80b
Lauren Emms <lauren.emms@guardian.co.uk>
2019-04-04 09:19:08 +0100

    add entities and key phrases

--- text-analysis/text-analyzer.go
+++ text-analysis/text-analyzer.go
@@ -17,17 +17,21 @@ import (
 	"github.com/pkg/errors"
 )
 
+// Entity will be split up into people, places, times, phrases
 type AnalysisResult struct {
 	DataSource           string
 	TopNegativeSentiment []SentimentResult
+	Entities             []EntityResult
+	KeyPhrases           []KeyPhrasesResult
 }
 
 func main() {
+	// pass the input file name and output file name
 
 	inputFileName := os.Args[1]
 	outputFileName := os.Args[2]
 
-	fileContent, err := ioutil.ReadFile(inputFileName) // just pass the file name
+	fileContent, err := ioutil.ReadFile(inputFileName)
 	if err != nil {
 		fmt.Print(err)
 	}
@@ -36,14 +40,26 @@ func main() {
 
 	client, err := CreateComprehendClient("identity")
 
-	analysisSentiment, err := AnalyseTextSentiment(client, fileContentAsString)
+	analyseSentiment, err := AnalyseTextSentiment(client, fileContentAsString)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	analyseEntities, err := AnalyseTextEntities(client, fileContentAsString)
+	if err != nil {
+		fmt.Print(err)
+	}
+
+	analyseKeyPhrases, err := AnalyseKeyPhrases(client, fileContentAsString)
 	if err != nil {
 		fmt.Print(err)
 	}
 
 	result := AnalysisResult{
 		DataSource:           inputFileName,
-		TopNegativeSentiment: analysisSentiment,
+		TopNegativeSentiment: analyseSentiment,
+		Entities:             analyseEntities,
+		KeyPhrases:           analyseKeyPhrases,
 	}
 
 	file, _ := json.MarshalIndent(result, "", " ")
@@ -101,4 +117,67 @@ func AnalyseSentenceSentiment(client *comprehend.Comprehend, sentence string) (S
 	input.SetTextList([]*string{aws.String(sentence)})
 	result, err := client.BatchDetectSentiment(input)
 	return SentimentResult{sentence, result.ResultList[0].SentimentScore.Negative}, err
+}
+
+// if we need to calculate the frequency of an array of strings
+
+//func dup_count(entityList []string) map[string]int {
+//
+//	duplicate_frequency := make(map[string]int)
+//
+//	for _, entityText := range entityList {
+//		// check if the item/element exist in the duplicate_frequency map
+//
+//		_, exist := duplicate_frequency[entityText]
+//
+//		if exist {
+//			duplicate_frequency[entityText] += 1 // increase counter by 1 if already in the map
+//		} else {
+//			duplicate_frequency[entityText] = 1 // else start counting from 1
+//		}
+//	}
+//	return duplicate_frequency
+//}
+
+type EntityResult struct {
+	Text string
+	Type string
+}
+
+func AnalyseTextEntities(client *comprehend.Comprehend, text string) ([]EntityResult, error) {
+	var entityTextArray []EntityResult
+	input := &comprehend.BatchDetectEntitiesInput{}
+	input.SetLanguageCode("en")
+	input.SetTextList([]*string{aws.String(text)})
+	entities, err := client.BatchDetectEntities(input)
+	if err != nil {
+		return entityTextArray, err
+	}
+	for _, entity := range entities.ResultList[0].Entities {
+		entityTextArray = append(entityTextArray, EntityResult{Text: *entity.Text, Type: *entity.Type})
+	}
+	return entityTextArray, err
+}
+
+//func AnalyseEntity(client *comprehend.Comprehend, text string) (EntityResult, error) {
+//
+//}
+
+type KeyPhrasesResult struct {
+	Text string
+}
+
+func AnalyseKeyPhrases(client *comprehend.Comprehend, text string) ([]KeyPhrasesResult, error) {
+	var keyPhrasesArray []KeyPhrasesResult
+	input := &comprehend.BatchDetectKeyPhrasesInput{}
+	input.SetLanguageCode("en")
+	input.SetTextList([]*string{aws.String(text)})
+	keyPhrases, err := client.BatchDetectKeyPhrases(input)
+	if err != nil {
+		return keyPhrasesArray, err
+	}
+	for _, keyPhrase := range keyPhrases.ResultList[0].KeyPhrases {
+		keyPhrasesArray = append(keyPhrasesArray, KeyPhrasesResult{Text: *keyPhrase.Text})
+	}
+	return keyPhrasesArray, err
 }
